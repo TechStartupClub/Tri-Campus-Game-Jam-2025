878c6c88d4c2a5148933fdbed6b94b272b4bb3f0
Matthew Franchini <matchewf11@gmail.com>
2025-04-11 16:12:32 -0700

    Refined the logic for the game, now looks for user inputs

--- view/game.go
+++ view/game.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"image/color"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
@@ -10,34 +11,49 @@ import (
 const (
 	ScreenWidth  = 800
 	ScreenHeight = 700
+	miniSize     = 400
 )
 
 type Game struct {
-	miniA *MiniGame
-	miniB *MiniGame
-	miniC *MiniGame
-	miniD *MiniGame
+	miniImage   *ebiten.Image
+	miniGameMap map[rune]*MiniGame
+	startTime   time.Time
 }
 
 type MiniGame struct {
-	alarmPos float32
-	gameType int
+	alarmPos  float32
+	miniImage *ebiten.Image
 }
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		g.miniImage = g.miniGameMap['w'].miniImage
+	} else if ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.miniImage = g.miniGameMap['a'].miniImage
+	} else if ebiten.IsKeyPressed(ebiten.KeyS) {
+		g.miniImage = g.miniGameMap['s'].miniImage
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.miniImage = g.miniGameMap['d'].miniImage
+	} else if g.miniImage == nil {
+		g.miniImage = ebiten.NewImage(miniSize, miniSize)
+		g.miniImage.Fill(color.RGBA{0, 0, 0, 255})
+	}
+	// need to do behavoir for each of the images/minigames
 	return nil
 }
 
-func DrawMiniGames(g *Game, screen *ebiten.Image) {
-	vector.DrawFilledRect(screen, g.miniA.alarmPos, 50, 75, 75, color.RGBA{255, 0, 0, 255}, false)
-	vector.DrawFilledRect(screen, g.miniB.alarmPos, 50, 75, 75, color.RGBA{255, 0, 0, 255}, false)
-	vector.DrawFilledRect(screen, g.miniC.alarmPos, 50, 75, 75, color.RGBA{255, 0, 0, 255}, false)
-	vector.DrawFilledRect(screen, g.miniD.alarmPos, 50, 75, 75, color.RGBA{255, 0, 0, 255}, false)
-}
-
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
-	DrawMiniGames(g, screen)
+	// draw backgroud
+	screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff}) // change this to a custom image
+	// draw game alerts
+	for _, miniGame := range g.miniGameMap {
+		vector.DrawFilledRect(screen, miniGame.alarmPos, 50, 75, 75, color.RGBA{0, 255, 0, 255}, false)
+	}
+	// draw mini image
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64((ScreenWidth-miniSize)/2), float64(ScreenHeight-miniSize)-100)
+	screen.DrawImage(g.miniImage, op)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -46,22 +62,25 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func NewGame() *Game {
 	game := Game{
-		miniA: &MiniGame{
-			alarmPos: 25,
-			gameType: 1,
-		},
-		miniB: &MiniGame{
-			alarmPos: 125,
-			gameType: 2,
-		},
-		miniC: &MiniGame{
-			alarmPos: 600,
-			gameType: 3,
-		},
-		miniD: &MiniGame{
-			alarmPos: 700,
-			gameType: 4,
-		},
+		miniGameMap: make(map[rune]*MiniGame, 4),
+		startTime:   time.Now(),
 	}
+	game.miniGameMap['w'] = &MiniGame{
+		alarmPos:  25,
+		miniImage: ebiten.NewImage(miniSize, miniSize),
+	}
+	game.miniGameMap['a'] = &MiniGame{
+		alarmPos:  125,
+		miniImage: ebiten.NewImage(miniSize, miniSize),
+	}
+	game.miniGameMap['s'] = &MiniGame{
+		alarmPos:  600,
+		miniImage: ebiten.NewImage(miniSize, miniSize),
+	}
+	game.miniGameMap['d'] = &MiniGame{
+		alarmPos:  700,
+		miniImage: ebiten.NewImage(miniSize, miniSize),
+	}
+
 	return &game
 }
