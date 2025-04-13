c2297008b6b29f23c14262fadae0181d3a447636
Matthew Franchini <matchewf11@gmail.com>
2025-04-13 11:59:09 -0700

    Fixed the structure of the project, making it easier to add logic.

--- model/updateMini.go
+++ model/updateMini.go
@@ -1,10 +1,55 @@
-// create interface with update()
-// then create a bunch of different games
-// like a tic tac toe struct that should implement the interface
-// give each miniGame one of teh game structs
+package model
 
-//package model
+import (
+	"image/color"
+	_ "image/png"
 
-//type UpdateableGame interface {
-//	updateMiniGame()
-//}
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type MiniGame interface {
+	UpdateGame()
+	DisplayAlert(*ebiten.Image)
+	DisplayScreen(*ebiten.Image, float64, float64)
+	GetMiniImage() *ebiten.Image
+}
+
+type BaseMiniGame struct {
+	AlarmPos  float32
+	MiniImage *ebiten.Image
+}
+
+func (game *BaseMiniGame) GetMiniImage() *ebiten.Image {
+	return game.MiniImage
+}
+
+func (game *BaseMiniGame) DisplayAlert(screen *ebiten.Image) {
+	vector.DrawFilledRect(screen, game.AlarmPos, 50, 75, 75, color.RGBA{0, 255, 0, 255}, false)
+}
+
+func (game *BaseMiniGame) DisplayScreen(screen *ebiten.Image, transX float64, transY float64) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(transX, transY)
+	screen.DrawImage(game.MiniImage, op)
+}
+
+func (game *BaseMiniGame) UpdateGame() {
+	// just default implementation fix later
+}
+
+type TicTacToeGame struct {
+	BaseMiniGame
+}
+
+type SortList struct {
+	BaseMiniGame
+}
+
+type CopyNumber struct {
+	BaseMiniGame
+}
+
+type UnscrabbleWord struct {
+	BaseMiniGame
+}
